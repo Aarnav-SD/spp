18feab60b5f4d3af94ffa5422caed45809e8fb0e
Aarnav-SD <aarnavsd0805@gmail.com>
2024-12-01 13:05:15 +0530

    BRO PLS MAN I NEED TO STUDY

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__, static_folder='dist')
-CORS(app)
+CORS(app, origins=["https://aarnav-sd.github.io"])
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
